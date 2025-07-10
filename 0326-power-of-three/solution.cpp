c46b4e900813dc0780971ab080490c3d5f00cc89
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2025-07-10 16:45:31 +0000

    Sync LeetCode submission Runtime - 4 ms (46.18%), Memory - 8.8 MB (75.23%)

---
+++ 0326-power-of-three/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1)
+        {
+            return true;
+        }
+        else if(n<3)
+        {
+            return false;
+        }
+        return n%3==0 && isPowerOfThree(n/3);
+    }
+};
