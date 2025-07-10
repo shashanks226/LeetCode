7eb6127ee5d1116a6fbf10e26ed9baf3ee89db01
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2025-07-10 17:30:26 +0000

    Sync LeetCode submission Runtime - 14 ms (22.37%), Memory - 7.8 MB (65.62%)

--- 1013-fibonacci-number/solution.cpp
+++ 1013-fibonacci-number/solution.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-map<int,int> cache;
     int fib(int n) {
-        if(n<=1) return n;
-        if(cache.find(n)!=cache.end())
+        if(n==0 || n==1)
         {
-            return cache[n];
+            return n;
         }
-        else {
-            //recurrence
-            cache[n]= fib(n-1)+fib(n-2);
-            return cache[n];
-        }
+        return fib(n-1)+fib(n-2);
     }
 };
