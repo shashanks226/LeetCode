766c51bd8adb5472e3ab36efb72c63ecd87dfe59
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2025-07-20 19:48:44 +0000

    Sync LeetCode submission Runtime - 2 ms (15.06%), Memory - 9.6 MB (62.54%)

---
+++ 0118-pascals-triangle/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> tri(numRows);
+        // tri[0][0]=1;
+        for(int i=0;i<numRows;i++)
+        {
+            tri[i].resize(i+1);
+            for(int j=0;j<=i;j++)
+            {
+                if(j==0 || j==i)
+                {
+                    tri[i][j]=1;
+                }
+                else
+                {
+                    tri[i][j]=tri[i-1][j-1]+tri[i-1][j];
+                }
+            }
+        }
+        return tri;
+    }
+};
