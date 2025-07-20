0e0f98cf60b0359e828a13587a4d5ab83722a8ab
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2025-07-20 11:04:24 +0000

    Sync LeetCode submission Runtime - 2 ms (5.10%), Memory - 8.7 MB (20.89%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-//bottom up approach
     int climbStairs(int n) {
         vector<int> dp(n+1);
-        if(n<=1) return n;
+        if(n==1|| n==2)
+        {
+            return n;
+        }
+        dp[0]=0;
         dp[1]=1;
         dp[2]=2;
         for(int i=3;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+           dp[i] = dp[i-1]+dp[i-2];
         }
         return dp[n];
     }
