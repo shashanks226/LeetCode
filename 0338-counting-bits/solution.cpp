abf8d9c1ebd2d564111c4a9a44d5ced2fa2fd23d
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2025-07-23 18:17:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.6 MB (57.51%)

--- 0338-counting-bits/solution.cpp
+++ 0338-counting-bits/solution.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> count;
-        for(int i=0;i<=n;i++)
-        {
-            int j=i;
-            int oc=0;
-            while(j>=1)
-            {
-                if(j%2==1)
-                {
-                    oc++;
-                }
-                j/=2;
-            }
-            count.push_back(oc);
+        vector<int> ans(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
-        return count;
+        return ans;
     }
 };
+
