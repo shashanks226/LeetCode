f156c02e6a51b4f9ce48c4b95ebe8b6b1607b762
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2026-03-14 11:46:53 +0000

    Sync LeetCode submission Runtime - 392 ms (95.70%), Memory - 0.0B (100.00%)

--- 1258-article-views-i/solution.sql
+++ 1258-article-views-i/solution.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-SELECT DISTINCT author_id AS id FROM Views
-WHERE author_id=viewer_id
-ORDER BY id ASC;
+select author_id as id from Views
+where author_id=viewer_id
+group by author_id
+Order by id ASC;
