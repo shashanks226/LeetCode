6f86f61e020c717aced64103a62288682003597e
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2026-03-18 09:37:32 +0000

    Sync LeetCode submission Runtime - 1335 ms (57.46%), Memory - 0.0B (100.00%)

--- 1153-product-sales-analysis-i/solution.sql
+++ 1153-product-sales-analysis-i/solution.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT p.product_name, s.year, s.price FROM Sales s
-JOIN Product p
-ON s.product_id = p.product_id
-order by sale_id;
+Select p.product_name, s.year, s.price from Sales s
+join product p on 
+s.product_id = p.product_id
