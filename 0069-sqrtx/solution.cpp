2f4b1afccf5a50bfe074621096feb4826eb344f3
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2026-05-31 11:52:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (14.27%)

---
+++ 0069-sqrtx/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int high=x;
+        int low = 0;
+        long long mid;
+        while(high>=low)
+        {
+            mid = (low+high)/2;
+            if(mid*mid == x)
+            {
+                return mid;
+            }
+            else if(mid*mid>x)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        return high;
+    }
+};
