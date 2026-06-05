eab4860a2b95955f76fd6ac66852fafe5ddbc1fc
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2026-06-05 00:55:10 +0000

    Sync LeetCode submission Runtime - 51 ms (41.70%), Memory - 29.1 MB (73.08%)

--- 0015-3sum/solution.cpp
+++ 0015-3sum/solution.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n= nums.size();
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        set<vector<int>> res;
-        for(int i=0;i<n-2;i++)
-        {
-            if(i > 0 && nums[i] == nums[i-1])
-            {
+        vector<vector<int>> res;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            unordered_map<int, int> map;
-            int target = -(nums[i]);
-            for(int j=i+1; j<n;j++)
+            for (int j = i + 1, k = n - 1; k > j;) 
             {
-                if(map.count(target- nums[j]))
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) 
                 {
-                    vector<int> temp= {nums[i],
-                    nums[j],
-                    target- nums[j]};
-
-                    sort(temp.begin(), temp.end());
-                    res.insert(temp);
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    while(k>j && nums[j] == nums[j+1]) j++;
+                    while(k>j && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
+                } 
+                else if (sum < 0) 
+                {
+                    j++;
+                } else {
+                    k--;
                 }
-                map[nums[j]]+=1;
             }
         }
-        return vector<vector<int>>(res.begin(), res.end());
+        return res;
     }
 };
