d1840acd49e4ce6d99db6c65570ab95c7f3b67dc
shashanks226 <115041758+shashanks226@users.noreply.github.com>
2026-06-10 16:32:26 +0000

    Sync LeetCode submission Runtime - 57 ms (34.93%), Memory - 50.9 MB (19.00%)

---
+++ 0560-subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        map[0] = 1;
+        int prefix=0;
+        int ans = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            prefix += nums[i];
+            ans += (map[prefix - k]);
+            map[prefix] += 1;
+        }
+        return ans;
+    }
+};
